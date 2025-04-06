0467b7cf0716d12aa6aa70527be1966c947d19ba
Paul <piratium3@gmail.com>
2025-04-06 15:42:18 +0300

    first c# console app

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 5;
+            double myDouble = 5.6;
+            string myString = "Hello, World!";
+            float myFloat = 5.7f;
+            decimal myDecimal = 5.8m;
+            Console.WriteLine(myString);
+        }
+    }
+}
