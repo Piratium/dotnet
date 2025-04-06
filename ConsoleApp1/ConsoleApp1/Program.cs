cd17882a8318d5ce827f63d85e26fbc645f9b830
Paul <piratium3@gmail.com>
2025-04-06 17:26:33 +0300

    Math

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,17 +1,20 @@
-﻿using System.Reflection;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int myInt = 5;
-            double myDouble = 5.6;
-            string myString = "Hello, World!";
-            float myFloat = 5.7f;
-            decimal myDecimal = 5.8m;
-            Console.WriteLine(myString);
+            int a = 2, b = 5;
+            Console.WriteLine(Math.Max(a, b));
+            Console.WriteLine($"Math.Floor(2.7) - {Math.Floor(2.7)}");
+            Console.WriteLine($"Math.Ceiling(2.3) - {Math.Ceiling(2.3)}");
+            Console.WriteLine($"Math.Round(2.5) - {Math.Round(2.5)}");
+
+            double angle = Math.Sin(Math.PI / 2);
+            Console.WriteLine(angle);
+
+            Console.WriteLine($"2 в степени 3 - {Math.Pow(2, 3)}");
+            Console.WriteLine($"Квадратный корень из 16 - {Math.Sqrt(16)}");
         }
     }
 }
