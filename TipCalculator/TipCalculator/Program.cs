7e089fc21f57fc2dbaaafd307142bf1d4200985a
Paul <piratium3@gmail.com>
2025-04-07 13:59:51 +0300

    Добавление проверки парсинга введенных данных

--- TipCalculator/TipCalculator/Program.cs
+++ TipCalculator/TipCalculator/Program.cs
@@ -2,16 +2,30 @@
 {
     internal class Program
     {
+        static void ParseResult(bool isParse)
+        {
+            if (!isParse)
+            {
+                Console.WriteLine("Данные введены некорректно!");
+                Environment.Exit(1);
+            }
+        } 
         static void Main(string[] args)
         {
             Console.Write("Введите сумму счета (руб): ");
-            decimal billAmount = Convert.ToDecimal(Console.ReadLine());
+            decimal billAmount;
+            bool IsBillAmountParse = decimal.TryParse(Console.ReadLine(), out billAmount);
+            ParseResult(IsBillAmountParse);
 
             Console.Write("Введите процент чаевых: ");
-            decimal tipsPercent = Convert.ToDecimal(Console.ReadLine());
+            decimal tipsPercent;
+            bool IsTipsPercentParse = decimal.TryParse(Console.ReadLine(), out tipsPercent);
+            ParseResult(IsTipsPercentParse);
 
             Console.Write("Введите кол-во людей: ");
-            int numOfPeople = Convert.ToInt32(Console.ReadLine());
+            int numOfPeople;
+            bool IsNumOfPeopleParse = int.TryParse(Console.ReadLine(), out numOfPeople);
+            ParseResult(IsNumOfPeopleParse);
 
             // Чаевые в общем 
             decimal tips = Math.Round((tipsPercent / 100) * billAmount, 2);
