3602034dac5f8d0a1f160b0a388f1833dc343fa7
Paul <piratium3@gmail.com>
2025-04-07 12:16:58 +0300

    finish

---
+++ TipCalculator/TipCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TipCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите сумму счета (руб): ");
+            decimal billAmount = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Введите процент чаевых: ");
+            decimal tipsPercent = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Введите кол-во людей: ");
+            int numOfPeople = Convert.ToInt32(Console.ReadLine());
+
+            // Чаевые в общем
+            decimal tips = Math.Round((tipsPercent / 100) * billAmount, 2);
+            Console.WriteLine($"Чаевые: {tips} руб");
+
+            // Счет на человека с учетом чаевых
+            decimal billPerPerson = Math.Round((tips + billAmount) / numOfPeople, 2);
+            Console.WriteLine($"Счет на человека: {billPerPerson} руб");
+        }
+    }
+}
