db522f43206a9dc89c91fbe52754694814128137
Paul <piratium3@gmail.com>
2025-04-08 15:15:14 +0300

    Многомерные/зубчатые массивы,

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] multiArray = new int[2,3] { { 2, 3, 1 }, { 1, 2, 3 } };
+            Console.WriteLine("Двумерный массив:");
+            for (int i = 0; i < multiArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < multiArray.GetLength(1); j++)
+                {
+                    Console.Write(multiArray[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            // Зубчатые массивы
+            int[][] jaggedArray = new int[3][];
+            jaggedArray[0] = new int[3] { 14, 23, 51 };
+            jaggedArray[1] = new int[2] { 81, 34 };
+            jaggedArray[2] = new int[] { 17, 11, 94, 21 };
+            Console.WriteLine("Зубчатый массив:");
+            foreach (int[] arr in jaggedArray)
+            {
+                foreach (int element in arr)
+                {
+                    Console.Write(element + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
