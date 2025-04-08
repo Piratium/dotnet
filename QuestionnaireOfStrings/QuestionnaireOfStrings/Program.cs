86a5ae73ed0b2cef3e29b28953b1b117f3ad5c50
Paul <piratium3@gmail.com>
2025-04-08 13:15:26 +0300

    Использование некоторых методов string на практике

---
+++ QuestionnaireOfStrings/QuestionnaireOfStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace QuestionnaireOfStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Пройди пару загадок\n");
+
+            // Первая загадка
+            Console.Write("Висит груша - нельзя скушать: ");
+            string answer = Console.ReadLine();
+            Console.WriteLine((answer.ToLower().Trim() == "груша") ? "Верно!" : "Неверно:(");
+
+            // Вторая загадка
+            Console.Write("Весной и летом одним цветом: ");
+            answer = Console.ReadLine();
+            if (answer.Contains("е")) answer = answer.Replace("е", "ё");
+            Console.WriteLine((answer.ToLower().Trim() == "ёлка") ? "Верно!" : "Неверно:(");
+        }
+    }
+}
