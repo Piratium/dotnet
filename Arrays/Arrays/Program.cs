daa1d5ddf3844026384dde9fb5e96d32592d7411
Paul <piratium3@gmail.com>
2025-04-08 16:07:39 +0300

    Коллекция List<T>

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -4,8 +4,16 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Одномерный массив:");
+            int[] myArray = new int[] { 1, 2, 3, 4 };
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write(myArray[i] + " ");
+            }
+            Console.WriteLine();
+
             int[,] multiArray = new int[2,3] { { 2, 3, 1 }, { 1, 2, 3 } };
-            Console.WriteLine("Двумерный массив:");
+            Console.WriteLine("\nДвумерный массив:");
             for (int i = 0; i < multiArray.GetLength(0); i++)
             {
                 for (int j = 0; j < multiArray.GetLength(1); j++)
@@ -15,12 +23,11 @@
                 Console.WriteLine();
             }
 
-            // Зубчатые массивы
             int[][] jaggedArray = new int[3][];
             jaggedArray[0] = new int[3] { 14, 23, 51 };
             jaggedArray[1] = new int[2] { 81, 34 };
             jaggedArray[2] = new int[] { 17, 11, 94, 21 };
-            Console.WriteLine("Зубчатый массив:");
+            Console.WriteLine("\nЗубчатый массив:");
             foreach (int[] arr in jaggedArray)
             {
                 foreach (int element in arr)
---
+++ List/List/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Коллекция лист:");
+            List<int> list = new() { 1, 2, 3, 4, 5, 6, 7 };
+            list.Add(78);
+            //list.Remove(2);
+            //list.RemoveAt(list.Count - 1);
+            string myList = string.Join(", ", list);
+            Console.WriteLine(myList);
+
+        }
+    }
+}
