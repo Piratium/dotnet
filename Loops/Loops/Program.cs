f1881aba043f271e72b3f42bf80daecb60af7cba
Paul <piratium3@gmail.com>
2025-04-08 14:47:19 +0300

    База про циклы

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Цикл while");
+            int iWhile = 0;
+            while (iWhile < 5)
+            {
+                Console.WriteLine($"Значение i: {iWhile}");
+                iWhile++;
+            }
+
+            Console.WriteLine("\nЦикл for");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Значение i: {i}");
+            }
+
+            // foreach только для чтения
+            Console.WriteLine("\nЦикл foreach");
+            string helloWrld = "Hello, World!";
+            foreach (char symbol in helloWrld)
+            {
+                Console.WriteLine(symbol);
+            }
+        }
+    }
+}
