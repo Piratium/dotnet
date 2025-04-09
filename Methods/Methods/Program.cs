ead667d504a210c0efbfab5f1ce6fa0e8f71f80d
Paul <piratium3@gmail.com>
2025-04-09 18:04:10 +0300

    Методы

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void A() { Console.WriteLine("Hi"); }
+            void B(string name) { Console.WriteLine($"My name is {name}"); }
+            double C() { return Math.Pow(10, 10); }
+            double D(double x, int y) { return x + y; }
+            A();
+            B("Paul");
+            Console.WriteLine(C());
+            Console.WriteLine(D(5, 2));
+            Console.WriteLine(D(C(),101010101));
+        }
+    }
+}
