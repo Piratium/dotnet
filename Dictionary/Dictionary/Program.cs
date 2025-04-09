d62b3c61eba6d3dd6c169ddec0acbde6b4940c43
Paul <piratium3@gmail.com>
2025-04-09 13:19:10 +0300

    Словарь

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Коллекция словарь:");
+            Dictionary<string, int> ages = new()
+            {
+                { "Bob", 20 },
+                { "Paul", 22 },
+                { "Sonya", 23 }
+            };
+            foreach (var pair in ages)
+            {
+                Console.WriteLine($"Имя: {pair.Key}, Возраст: {pair.Value}");
+            }
+            Console.WriteLine();
+            // Удаления всех пар
+            ages.Clear();
+            foreach (var pair in ages)
+            {
+                Console.WriteLine($"Имя: {pair.Key}, Возраст: {pair.Value}");
+            }
+            // Добавление пар
+            ages.Add("Hi", 2);
+            ages.Add("Bye", 4);
+            ages.Add("Goodbye", 6);
+            ages.Add("Goodhi", 6);
+            ages.Add("Hell", 6);
+            foreach (var pair in ages)
+            {
+                Console.WriteLine($"Имя: {pair.Key}, Возраст: {pair.Value}");
+            }
+
+            //Удаление происходит по ключу пары
+            ages.Remove("Bye");
+
+            Console.WriteLine("\nПребор только ключей");
+            foreach (var item in ages.Keys)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\nПребор только значений");
+            foreach (var item in ages.Values)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- List/List/Program.cs
+++ List/List/Program.cs
@@ -5,13 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Коллекция лист:");
-            List<int> list = new() { 1, 2, 3, 4, 5, 6, 7 };
+            List<int> list = new() { 1, 2, 3, 4, 5 };
             list.Add(78);
-            //list.Remove(2);
-            //list.RemoveAt(list.Count - 1);
+            list.Add(15);
+            list.Remove(2);
+            list.RemoveAt(list.Count - 1);
             string myList = string.Join(", ", list);
             Console.WriteLine(myList);
-
         }
     }
 }
