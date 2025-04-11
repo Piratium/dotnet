b84dc55bd3ec6a969b4f161978d0f730f89cc450
Paul <piratium3@gmail.com>
2025-04-11 13:11:06 +0300

    Обработка исключений

---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            try
+            {
+                Console.WriteLine(array[12]);
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine("Какая-то ошибка: " + ex.Message);
+            }
+        }
+    }
+}
